7ceb08039efaf51d4cb9e15663fa43953aa0c4c0
mcampora <mcampora@gmail.com>
2022-11-17 11:59:40 +0100

    added a github workflow

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-export TARGET=lazgar-prod
-
-aws --profile $TARGET \
-    cloudformation deploy \
+# use current AWS credentials
+aws cloudformation deploy \
     --capabilities CAPABILITY_NAMED_IAM \
     --stack-name lazgar \
     --template-file ./cfn-pipeline.yaml \
