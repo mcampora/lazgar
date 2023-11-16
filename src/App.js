4057a745c27a8c92977bbf6ea03187aa8a166620
mcampora <mcampora@gmail.com>
2023-11-16 09:05:27 +0100

    added alt property to img

--- src/App.js
+++ src/App.js
@@ -195,9 +195,9 @@ function App() {
             />
           </Grid>
           <Grid>
-          <img className={classes.badge} src="ccp.png"/>
-          <img className={classes.badge} src="csap.png"/>
-          <img className={classes.badge} src="css.png"/>
+          <img className={classes.badge} src="ccp.png" alt="ccp"/>
+          <img className={classes.badge} src="csap.png" alt="csap"/>
+          <img className={classes.badge} src="css.png" alt="css"/>
           </Grid>
           <Grid className={classes.item} item xs={12}>
             <Typography variant="caption">Photo by <a href="https://unsplash.com/@bady?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">bady abbas</a>, <a href="https://unsplash.com/@lucabravo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Luca Bravo</a> and <a href="https://unsplash.com/@joelfilip?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Joel Filipe</a> on <a href="https://unsplash.com/photos/white-cubby-shelf-hxi_yRxODNc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></Typography><br/>
